daf8a429f9891207859dc5af6f05d336ca8aef1f
TANTOMO Andrew-Getty[FRJP:Digital Business Transformation Services](Tantomo Andrew) <andrew-getty.tantomo@fastretailing.com>
2019-01-04 11:02:03 +0900

    shadow separation logic

--- Playground/TableShadowViewController.swift
+++ Playground/TableShadowViewController.swift
@@ -21,23 +21,23 @@ class LinkableTableView: UITableView {
         }
     }
 
-    override var dataSource: UITableViewDataSource? {
-        didSet {
-            linkedTableView?.dataSource = dataSource
-        }
-    }
+//    override var dataSource: UITableViewDataSource? {
+//        didSet {
+//            linkedTableView?.dataSource = dataSource
+//        }
+//    }
 
     var linkedTableView: UITableView?
 
 }
 
-class ShadowSeparableTableContainerView: UITableView {
+class ShadowSeparableTableContainerView: UIView {
     var tableView: LinkableTableView!
-    private var shadowTableView: UITableView!
+    var shadowTableView: UITableView!
     private var noShadowTableView: UITableView!
 
-    override init(frame: CGRect, style: UITableViewStyle) {
-        super.init(frame: frame, style: style)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         commonInit()
     }
 
@@ -47,7 +47,16 @@ class ShadowSeparableTableContainerView: UITableView {
     }
 
     func commonInit() {
-        tableView.isHidden = true
+        tableView = LinkableTableView(frame: frame)
+        shadowTableView = UITableView(frame: frame)
+        noShadowTableView = UITableView(frame: frame)
+
+//        tableView.isHidden = true
+        shadowTableView.backgroundColor = UIColor.clear
+        noShadowTableView.backgroundColor = UIColor.clear
+        shadowTableView.backgroundView = UIView()
+        noShadowTableView.backgroundView = UIView()
+
         tableView.linkedTableView = noShadowTableView
         shadowTableView.dataSource = self
         noShadowTableView.dataSource = self
@@ -55,12 +64,12 @@ class ShadowSeparableTableContainerView: UITableView {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         shadowTableView.translatesAutoresizingMaskIntoConstraints = false
         noShadowTableView.translatesAutoresizingMaskIntoConstraints = false
-        shadowTableView.isUserInteractionEnabled = false
+        shadowTableView.isUserInteractionEnabled = true
         noShadowTableView.isUserInteractionEnabled = false
 
         addSubview(tableView)
         addSubview(shadowTableView)
-        addSubview(noShadowTableView)
+//        addSubview(noShadowTableView)
 
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: topAnchor),
@@ -73,10 +82,10 @@ class ShadowSeparableTableContainerView: UITableView {
             shadowTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
             shadowTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
 
-            noShadowTableView.topAnchor.constraint(equalTo: topAnchor),
-            noShadowTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            noShadowTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            noShadowTableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+//            noShadowTableView.topAnchor.constraint(equalTo: topAnchor),
+//            noShadowTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+//            noShadowTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+//            noShadowTableView.trailingAnchor.constraint(equalTo: trailingAnchor)
             ])
     }
 
@@ -92,16 +101,17 @@ extension ShadowSeparableTableContainerView: UITableViewDataSource {
         guard let cell = self.tableView.cellForRow(at: indexPath) else {
             fatalError()
         }
-        if tableView == shadowTableView && cell is ShadowErasable {
+        let dupCell = self.tableView.dequeueReusableCell(withIdentifier: cell.reuseIdentifier!, for: indexPath)
+        if tableView == shadowTableView && dupCell.reuseIdentifier == "leaf" {
             cell.contentView.alpha = 0
-        } else if tableView == shadowTableView && !(cell is ShadowErasable) {
+        } else if tableView == shadowTableView && dupCell.reuseIdentifier != "leaf" {
             cell.contentView.alpha = 1
-        } else if tableView == noShadowTableView && cell is ShadowErasable {
+        } else if tableView == noShadowTableView && dupCell.reuseIdentifier == "leaf" {
             cell.contentView.alpha = 1
-        } else if tableView == noShadowTableView && !(cell is ShadowErasable) {
+        } else if tableView == noShadowTableView && dupCell.reuseIdentifier != "leaf" {
             cell.contentView.alpha = 0
         }
-        return cell
+        return dupCell
     }
 }
 
@@ -114,30 +124,67 @@ class TableShadowViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        shadowTableView.layer.masksToBounds = false
-//        shadowTableView.layer.shadowRadius = 2.0
-//        shadowTableView.layer.shadowColor = UIColor.lightGray.cgColor
-//        shadowTableView.layer.shadowOffset = CGSize(width: 2, height: 2)
-//        shadowTableView.layer.shadowOpacity = 0.8
-//
-//        shadowTableView.delegate = self
-//        plainTableView.delegate = self
-//
-//        shadowTableView.dataSource = self
-//        plainTableView.dataSource = self
+        let shadowNib = UINib(nibName: "ShadowAllowedTableViewCell", bundle: nil)
+        let noShadowNib = UINib(nibName: "ShadowProhibitedTableViewCell", bundle: nil)
+
+        shadowTableView.layer.masksToBounds = false
+        shadowTableView.layer.shadowRadius = 2.0
+        shadowTableView.layer.shadowColor = UIColor.lightGray.cgColor
+        shadowTableView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        shadowTableView.layer.shadowOpacity = 0.8
+
+        if #available(iOS 10.0, *) {
+            plainTableView.refreshControl = UIRefreshControl()
+            let ref = UIRefreshControl()
+            ref.alpha = 0
+            ref.addTarget(self, action: #selector(refreshControlValueChanged), for: UIControlEvents.valueChanged)
+            shadowTableView.refreshControl = ref
+        } else {
+            // Fallback on earlier versions
+        }
+
+        shadowTableView.delegate = self
+        plainTableView.delegate = self
+
+        shadowTableView.register(shadowNib, forCellReuseIdentifier: "shadow")
+        shadowTableView.register(noShadowNib, forCellReuseIdentifier: "noShadow")
+        plainTableView.register(shadowNib, forCellReuseIdentifier: "shadow")
+        plainTableView.register(noShadowNib, forCellReuseIdentifier: "noShadow")
+
+        shadowTableView.dataSource = self
+        plainTableView.dataSource = self
+
+//        plainTableView.allowsSelection = false
+//        shadowTableView.allowsSelection = false
+
+//        print(plainTableView.layer.shadowRadius)
+//        print(plainTableView.layer.shadowColor)
+//        print(plainTableView.layer.shadowOffset)
+//        print(plainTableView.layer.shadowOpacity)
 //
 //        shadowSeparableTableContainer.tableView
 
-        
 
-        shadowSeparableTableContainer.tableView.layer.masksToBounds = false
-        shadowSeparableTableContainer.tableView.layer.shadowRadius = 2.0
-        shadowSeparableTableContainer.tableView.layer.shadowColor = UIColor.lightGray.cgColor
-        shadowSeparableTableContainer.tableView.layer.shadowOffset = CGSize(width: 2, height: 2)
-        shadowSeparableTableContainer.tableView.layer.shadowOpacity = 0.8
+//        shadowSeparableTableContainer.tableView.register(nib, forCellReuseIdentifier: "leaf")
+//        shadowSeparableTableContainer.tableView.register(nib, forCellReuseIdentifier: "flower")
+//        shadowSeparableTableContainer.tableView.dataSource = self
+//
+//        shadowSeparableTableContainer.shadowTableView.layer.masksToBounds = false
+//        shadowSeparableTableContainer.shadowTableView.layer.shadowRadius = 2.0
+//        shadowSeparableTableContainer.shadowTableView.layer.shadowColor = UIColor.lightGray.cgColor
+//        shadowSeparableTableContainer.shadowTableView.layer.shadowOffset = CGSize(width: 2, height: 2)
+//        shadowSeparableTableContainer.shadowTableView.layer.shadowOpacity = 0.8
     }
 
 
+    @objc
+    func refreshControlValueChanged(_ sender: Any) {
+        if #available(iOS 10.0, *) {
+            plainTableView.refreshControl?.beginRefreshing()
+        } else {
+            // Fallback on earlier versions
+        }
+    }
 
 }
 
@@ -150,18 +197,45 @@ class LeafTableViewCell: UITableViewCell {
     //    @IBOutlet var titleLabel: UILabel!
 }
 
+class ShadowAllowedTableViewCell: UITableViewCell { }
+
+class ShadowProhibitedTableViewCell: UITableViewCell, ShadowErasable { }
+
 extension TableShadowViewController: UITableViewDelegate {
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        plainTableView.contentOffset = scrollView.contentOffset
+        plainTableView.contentOffset = scrollView.contentOffset
+
+//        if #available(iOS 10.0, *) {
+//            if plainTableView.contentOffset.y <= -(shadowTableView.refreshControl?.frame.height ?? 0) {
+//                plainTableView.refreshControl?.beginRefreshing()
+//            }
+////            print(shadowTableView.refreshControl?.frame)
+//        } else {
+//            // Fallback on earlier versions
+//        }
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row % 3 != 0 {
-            return 44.0
-        } else {
-            return 66.0
-        }
+//        if indexPath.row % 3 != 0 {
+            return 88.0
+//        } else {
+//            return 132.0
+//        }
+    }
+
+    func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+//        let isShadowAppliedToTable = tableView.layer.shadowOpacity != 0
+//        if !isShadowAppliedToTable {
+//            shadowTableView.highli
+//        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let isShadowAppliedToTable = tableView.layer.shadowOpacity != 0
+//        if !isShadowAppliedToTable {
+//            shadowTableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableViewScrollPosition.none)
+//        }
     }
 }
 
@@ -173,19 +247,20 @@ extension TableShadowViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell
+        let isShadowAppliedToTable = tableView.layer.shadowOpacity != 0
+
         if indexPath.row % 3 != 0 {
-            cell = tableView.dequeueReusableCell(withIdentifier: "flower", for: indexPath) as! FlowerTableViewCell
-//            if tableView == plainTableView {
-//                cell.contentView.alpha = 0
-//                cell.backgroundColor = .clear
-//            }
+            cell = tableView.dequeueReusableCell(withIdentifier: "shadow", for: indexPath) as! ShadowAllowedTableViewCell
+            if !isShadowAppliedToTable {
+                cell.contentView.alpha = 0
+            }
         } else {
-            cell = tableView.dequeueReusableCell(withIdentifier: "leaf", for: indexPath) as! LeafTableViewCell
-//            if tableView == shadowTableView {
-//                cell.contentView.alpha = 0
-////                cell.backgroundColor = .red
-//            }
+            cell = tableView.dequeueReusableCell(withIdentifier: "noShadow", for: indexPath) as! ShadowProhibitedTableViewCell
+            if isShadowAppliedToTable {
+                cell.contentView.alpha = 0
+            }
         }
+        cell.backgroundColor = .clear
         return cell
     }
 }
